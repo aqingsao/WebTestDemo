91824a65c5405463437cb6813020b2eb099eff1b
xqzhang <xqzhang@thoughtworks.com>
2012-03-24 21:56:30 +1100

    Now project works

--- src/main/java/com/telstra/webtest/controller/HelloController.java
+++ src/main/java/com/telstra/webtest/controller/HelloController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class HelloController {
-    @RequestMapping("/hello")
+    @RequestMapping("hello")
     public ModelAndView hello() {
         System.out.println("Hello----------");
         return new ModelAndView("hello");
+    }
+    @RequestMapping("/")
+    public void test(){
+        System.out.println("test-------");
     }
 }
