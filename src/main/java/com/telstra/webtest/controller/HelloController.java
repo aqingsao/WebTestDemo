bb51d396ddc802d0d52194ef1924e8fb487204b7
xqzhang <xqzhang@thoughtworks.com>
2012-03-24 21:12:51 +1100

    First import

---
+++ src/main/java/com/telstra/webtest/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.telstra.webtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        System.out.println("Hello----------");
+        return new ModelAndView("hello");
+    }
+}
