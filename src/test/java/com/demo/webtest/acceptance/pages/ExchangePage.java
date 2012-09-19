01fc966993ea03397982870e4347d25a643a7e8d
jacky li <veryfaint@gmail.com>
2012-09-19 23:21:20 +0800

    use pageobject pattern

--- src/test/java/com/demo/webtest/acceptance/pages/ExchangePage.java
+++ src/test/java/com/demo/webtest/acceptance/pages/ExchangePage.java
@@ -3,24 +3,37 @@ package com.demo.webtest.acceptance.pages;
 import com.demo.webtest.domain.Currency;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 public class ExchangePage extends BasePage {
+
+    @CacheLookup
+    private WebElement fromCurrency;
+
+    @FindBy(name = "fromAmount")
+    @CacheLookup
+    private WebElement fromAmountElement;
+
+    @CacheLookup
+    private WebElement toCurrency;
+
+    @FindBy(className = "errorMessage")
+    @CacheLookup
+    private WebElement errorMessage;
 
     @Override
     protected String getPath() {
         return "/exchange/";
     }
 
-    public ExchangeResultPage exchange(Currency fromCurrency, Currency toCurrency, double fromAmount) {
-        WebElement fromCurrencyElement = webDriver.findElement(By.name("fromCurrency"));
-        fromCurrencyElement.findElement(By.cssSelector("option[value=" + fromCurrency.name() + "]")).setSelected();
+    public ExchangeResultPage exchange(Currency fromCurrencyValue, Currency toCurrencyValue, double fromAmount) {
+        fromCurrency.findElement(By.cssSelector("option[value=" + fromCurrencyValue.name() + "]")).setSelected();
 
-        WebElement fromAmountElement = webDriver.findElement(By.name("fromAmount"));
         fromAmountElement.clear();
         fromAmountElement.sendKeys(String.valueOf(fromAmount));
 
-        WebElement toCurrencyElement = webDriver.findElement(By.name("toCurrency"));
-        toCurrencyElement.findElement(By.cssSelector("option[value=" + toCurrency.name() + "]")).setSelected();
+        toCurrency.findElement(By.cssSelector("option[value=" + toCurrencyValue.name() + "]")).setSelected();
 
         webDriver.findElement(By.cssSelector("form")).submit();
 
@@ -28,7 +41,6 @@ public class ExchangePage extends BasePage {
     }
 
     public boolean hasErrorMessage(String message) {
-        String text = webDriver.findElement(By.cssSelector("div.errorMessage")).getText();
-        return text.contains(message);
+        return errorMessage.getText().contains(message);
     }
 }
--- src/test/java/com/demo/webtest/acceptance/pages/ExchangeResultPage.java
+++ src/test/java/com/demo/webtest/acceptance/pages/ExchangeResultPage.java
@@ -2,15 +2,19 @@ package com.demo.webtest.acceptance.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 public class ExchangeResultPage extends BasePage {
+    @FindBy(className = "toAmount")
+    private WebElement toAmount;
+
     @Override
     protected String getPath() {
         return "/exchange";
     }
 
     public double getToAmount() {
-        WebElement element = webDriver.findElement(By.cssSelector(".toAmount"));
-        return Double.valueOf(element.getText());
+        return Double.valueOf(toAmount.getText());
     }
 }
